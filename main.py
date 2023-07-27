f87776175540f3a897a648d32beb04ebdea0b56c
manuo <ojhapranav1999@gmail.com>
2023-07-27 21:34:37 +0530

    Day 31 : Some more coding in flask , adding a couple of url endpoints .

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask import render_template
 import pandas as pd
 
 app = Flask(__name__)
+
+stations = pd.read_csv("Data_sets/stations.txt" , skiprows=17)
+
+variable = "Hello there"
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",data = stations)
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
@@ -18,6 +22,21 @@ def about(station,date):
             "date":date,
             "temperature":temperature
     }
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    df = pd.read_csv("Data_sets/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20, parse_dates=["    DATE"])
+    results = df.to_dict(orient = "records")
+    return results
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    df = pd.read_csv("Data_sets/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient = "records")
+    return result
+
 
 if __name__=="__main__":
     app.run(debug=True)
